a1509e73c473eb32536482e486752f434b160ec5
KaveyaAnnadurai <kaveya.a@m2pfintech.com>
2023-03-31 07:54:47 +0530

    Requirement 2: Included logic for fizzbuzz second requirement.

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -4,16 +4,26 @@ public class FizzBuzz {
     private int number;
 
     public FizzBuzz(int number){
-        if(number > 100 || number < 1)
-            throw new IllegalArgumentException();
         this.number = number;
     }
+    public boolean toCheckIfDigitIsPresentInNumber(int number, int digit){
+        int unitsDigit;
+        while(number > 0){
+            unitsDigit = number%10;
 
+            if(unitsDigit == digit)
+                return true;
+        }
+        return false;
+    }
     public String checkFizzBuzz(){
-
-        if( number % 3 == 0)
+        if(number > 100 || number < 1)
+            throw new IllegalArgumentException();
+        if(number % 3 ==0 && number % 5 == 0)
+            return "fizzbuzz";
+        if( number % 3 == 0 || toCheckIfDigitIsPresentInNumber(number,3))
             return "fizz";
-        else if(number % 5 == 0)
+        else if(number % 5 == 0 || toCheckIfDigitIsPresentInNumber(number,5))
             return "buzz";
         else return String.valueOf(number);
     }
--- src/test/java/com/m2p/FizzBuzzTest.java
+++ src/test/java/com/m2p/FizzBuzzTest.java
@@ -4,11 +4,12 @@ import org.example.FizzBuzz;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FizzBuzzTest {
 
     @Test
-    void ifNumberDivisibleByThreeReturnFizz(){
+    void ifNumberDivisibleByThreeOrHasThreeReturnFizz(){
 
         //arrange
         FizzBuzz fizzBuzz = new FizzBuzz(6);
@@ -22,7 +23,21 @@ public class FizzBuzzTest {
     }
 
     @Test
-    void ifNumberDivisibleByFiveReturnBuzz(){
+    void ifNumberDivisibleByThreeOrHasThreeReturnFizzForThirtyThree(){
+
+        //arrange
+        FizzBuzz fizzBuzz = new FizzBuzz(33);
+        String expectedValue = "fizz";
+
+        //act
+        String actualValue = fizzBuzz.checkFizzBuzz();
+
+        //assert
+        assertEquals( expectedValue, actualValue);
+    }
+
+    @Test
+    void ifNumberDivisibleByFiveOrHasFiveReturnBuzz(){
 
         //arrange
         FizzBuzz fizzBuzz = new FizzBuzz(25);
@@ -34,6 +49,21 @@ public class FizzBuzzTest {
         //assert
         assertEquals( expectedValue, actualValue);
     }
+
+    @Test
+    void ifNumberDivisibleByFiveOrHasFiveReturnBuzzForFiftyFive(){
+
+        //arrange
+        FizzBuzz fizzBuzz = new FizzBuzz(55);
+        String expectedValue = "buzz";
+
+        //act
+        String actualValue = fizzBuzz.checkFizzBuzz();
+
+        //assert
+        assertEquals( expectedValue, actualValue);
+    }
+
 
     @Test
     void ifNumberNotDivisibleByFiveOrThreeReturnNumber(){
@@ -49,6 +79,24 @@ public class FizzBuzzTest {
         assertEquals( expectedValue, actualValue);
     }
 
+    @Test
+    void ifNumberDivisibleByBothThreeAndFiveReturnFizzBuzz(){
+        //arrange
+        FizzBuzz fizzBuzz = new FizzBuzz(15);
+        String expectedValue = "fizzbuzz";
 
+        //act
+        String actualValue = fizzBuzz.checkFizzBuzz();
+
+        //assert
+        assertEquals( expectedValue, actualValue);
+    }
+    @Test
+    void ifNumberNotInRangeOneToHundredThrowException(){
+        FizzBuzz fizzBuzz2 = new FizzBuzz(1000);
+
+        assertThrows(IllegalArgumentException.class, () ->
+                fizzBuzz2.checkFizzBuzz());
+    }
 
 }
